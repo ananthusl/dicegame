e33113331189282dc38fda74be28b556181fae62
ananthusl <93879821+ananthusl@users.noreply.github.com>
2022-02-15 21:50:04 +0530

    Uploaded the necessary files

---
+++ dicegame.js
@@ -0,0 +1,66 @@
+
+
+function dicegame1(){
+    var randomvalue1;
+    randomvalue1=Math.floor(Math.random()*6)+1;
+    if (randomvalue1===1){
+      document.getElementsByClassName('img1')[0].setAttribute('src','images/dice1.png');
+
+    }
+    else if(randomvalue1===2){
+      document.getElementsByClassName('img1')[0].setAttribute('src','images/dice2.png');
+
+    }
+    else if(randomvalue1===3){
+      document.getElementsByClassName('img1')[0].setAttribute('src','images/dice3.png');
+
+    }
+    else if(randomvalue1===4){
+      document.getElementsByClassName('img1')[0].setAttribute('src','images/dice4.png');
+
+    }
+    else if(randomvalue1===5){
+      document.getElementsByClassName('img1')[0].setAttribute('src','images/dice5.png');
+
+    }
+    else{
+      document.getElementsByClassName('img1')[0].setAttribute('src','images/dice6.png');
+
+    }
+    dicegame2(randomvalue1);
+  }
+
+function dicegame2(randomvalue1){
+    var randomvalue2;
+    randomvalue2=Math.floor(Math.random()*6)+1;
+    if (randomvalue2===1){
+      document.getElementsByClassName('img2')[0].setAttribute('src','images/dice1.png');
+    }
+    else if(randomvalue2===2){
+      document.getElementsByClassName('img2')[0].setAttribute('src','images/dice2.png');
+    }
+    else if(randomvalue2===3){
+      document.getElementsByClassName('img2')[0].setAttribute('src','images/dice3.png');
+    }
+    else if(randomvalue2===4){
+      document.getElementsByClassName('img2')[0].setAttribute('src','images/dice4.png');
+    }
+    else if(randomvalue2===5){
+      document.getElementsByClassName('img2')[0].setAttribute('src','images/dice5.png');
+    }
+    else{
+      document.getElementsByClassName('img2')[0].setAttribute('src','images/dice6.png');
+    }
+    result(randomvalue1,randomvalue2);
+  }
+function result(randomvalue1,randomvalue2){
+    if (randomvalue1>randomvalue2){
+      document.querySelector('h1').innerHTML='Player 1 wins';
+    }
+    else if (randomvalue2>randomvalue1){
+      document.querySelector('h1').innerHTML='Player 2 wins';
+    }
+    else{
+      document.querySelector('h1').innerHTML='Match is draw';
+    }
+  }
